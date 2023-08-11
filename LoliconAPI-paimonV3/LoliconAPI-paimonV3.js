bf85264d48302b66046024eba2dbc2faf47aa552
misaka20002 <40714502+misaka20002@users.noreply.github.com>
2023-08-11 23:06:11 +0800

    Update LoliconAPI-paimonV3.js

--- LoliconAPI-paimonV3/LoliconAPI-paimonV3.js
+++ LoliconAPI-paimonV3/LoliconAPI-paimonV3.js
@@ -35,7 +35,7 @@ export class LoliconAPI extends plugin {
             /** https://oicqjs.github.io/oicq/#events */
             event: 'message',
             /** 优先级，数字越小等级越高 */
-            priority: 450,
+            priority: 50,
             rule: [
                 {
                     /** 命令正则匹配 */
