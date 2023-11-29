2d67c02d6f5688ae902b5c498663e771fe844970
misaka20002 <40714502+misaka20002@users.noreply.github.com>
2023-11-29 13:44:59 +0800

    Update 修复椰奶代理.js

--- psign/修复椰奶代理.js
+++ psign/修复椰奶代理.js
@@ -1,4 +1,5 @@
 // 修复椰奶代理
+// curl -# -L -o "./plugins/yenai-plugin/lib/request/httpsProxyAgentMod.js" "https://raw.githubusercontent.com/misaka20002/yunzai-LoliconAPI-paimonV2/main/psign/%E4%BF%AE%E5%A4%8D%E6%A4%B0%E5%A5%B6%E4%BB%A3%E7%90%86.js"
 
 import HttpsProxyAgentOrig from 'https-proxy-agent'
 
