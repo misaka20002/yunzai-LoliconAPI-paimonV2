a8425696c4d1f078c0629a612260d7e5b0db7f2b
misaka20002 <40714502+misaka20002@users.noreply.github.com>
2023-12-11 00:34:46 +0800

    Update psign.sh

--- psign/psign.sh
+++ psign/psign.sh
@@ -234,9 +234,9 @@ echo -e  ${green} 7.  ${cyan}HD: 8.9.73${background}
 echo -e  ${green} 8.  ${cyan}HD: 8.9.76${background}
 echo -e  ${green} 9.  ${cyan}HD: 8.9.80${background}
 echo -e  ${green}10.  ${cyan}HD: 8.9.83${background}
-echo -e  ${green}11.  ${cyan}HD: 8.9.85（有bug禁用）${background}
-echo -e  ${green}b.  ${cyan}HD: 8.9.88（需要icqq0.6.2）${background}
-echo -e  ${green}c.  ${cyan}HD: 8.9.90（需要icqq0.6.3）${background}
+echo -e  ${green}11.  ${cyan}HD: 8.9.85（需要icqq0.6.2）${background}
+echo -e  ${green}12.  ${cyan}HD: 8.9.88（需要icqq0.6.2）${background}
+echo -e  ${green}13.  ${cyan}HD: 8.9.90（需要icqq0.6.3）${background}
 echo -e  ${green}d.  ${cyan}HD: 8.9.93（需要icqq0.6.5）${background}
 echo "========================="
 echo -en ${green}请输入您的选项: ${background};read num
@@ -274,10 +274,10 @@ export version=8.9.83
 11|8.9.85)
 export version=8.9.85
 ;;
-b|8.9.88)
+12|8.9.88)
 export version=8.9.88
 ;;
-c|8.9.90)
+13|8.9.90)
 export version=8.9.90
 ;;
 d|8.9.93)
